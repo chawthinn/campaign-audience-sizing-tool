e01655af2cdd64415637954aff4b07fad543578c
ChawThinn <c0916347@mylambton.ca>
2026-05-24 18:38:09 -0400

    Update TMP_DIR path to use absolute directory for app data storage

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,8 @@ from fastapi.responses import FileResponse
 
 
 BASE_DIR = Path(__file__).resolve().parent
-TMP_DIR = BASE_DIR / 'tmp'
+# TMP_DIR = BASE_DIR / 'tmp'
+TMP_DIR = Path('/tmp/app_data')
 TMP_DIR.mkdir(parents=True, exist_ok=True)
 JOBS_DIR = TMP_DIR / 'jobs'
 JOBS_DIR.mkdir(parents=True, exist_ok=True)
